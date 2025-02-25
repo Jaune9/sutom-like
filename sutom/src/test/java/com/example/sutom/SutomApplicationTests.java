7edb7cb577516981d7976975415accc99e4bf983
Jaune9 <47558505+Jaune9@users.noreply.github.com>
2025-02-25 10:36:30 +0100

    Yay ca marche le Hello World

---
+++ sutom/src/test/java/com/example/sutom/SutomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sutom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SutomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
