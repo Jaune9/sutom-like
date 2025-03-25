60d8c1eb5c12399225530c490414ce0e1641e05a
Jaune9 <jonathan.triqueneau@gmail.com>
2025-03-25 12:12:27 +0100

    truc qui pète mais fixable, gotago

--- sutombis-main/src/main/java/com/example/sutombis/model/Word.java
+++ sutombis-main/src/main/java/com/example/sutombis/model/Word.java
@@ -5,6 +5,7 @@ public class Word {
     private String name;
     private String categorie;
     private String blurredWord;
+    private String spacedWord;
 
   public long getId() {
     return id;
@@ -34,8 +35,23 @@ public class Word {
     return blurredWord;
   }
 
-  public void setBlurredWord(String blurredWord){
-    this.blurredWord = blurredWord;
+  public void setBlurredWord(String wordToBlur){
+    this.blurredWord = wordToBlur;
+  }
+
+  public String getSoaceWord() {
+    return spacedWord;
+  }
+
+  public void setSpacedWord(String wordToSpace){
+    this.spacedWord = wordToSpace;
+  }
+
+  public String spaceWord(String wordToSpace){
+    if (wordToSpace == null)
+      return "";
+    String spaced_hidden_word = wordToSpace.replace("", " ").trim();
+    return spaced_hidden_word;
   }
 
   public String blurWord(String wordToBlur){
@@ -43,9 +59,8 @@ public class Word {
     int length = wordToBlur.length();
     String hidden_daily_word = String.format("%-" + length + "s", wordToBlur.charAt(0)).replace(' ', '_');
     setBlurredWord(hidden_daily_word);
-    return hidden_daily_word;
+    spaceWord(this.blurredWord);
+    return this.spacedWord;
   }
 
-
-  
 }
